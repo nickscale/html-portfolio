d607fb7557551d867344c4b9a4a33a3cdeca2662
Nick S <nickswinglehurst@gmail.com>
2023-06-20 15:49:04 +0100

    TTT working

--- tiictaactooe/tiictaactooe.js
+++ tiictaactooe/tiictaactooe.js
@@ -1,129 +1,144 @@
-const defaultBoard = 
-[['', '', '', ''],
-['', '', '', ''],
-['', '', '', ''],
-['', '', '', '']];
+const defaultBoard = [
+  ["-", "-", "-", "-"],
+  ["-", "-", "-", "-"],
+  ["-", "-", "-", "-"],
+  ["-", "-", "-", "-"],
+];
 const swatch = ["#06D6A0", "#1B9AAA", "#EF476F", "#FFC43D", "#F8FFE5"];
 const Colors = ["color1", "color2", "color3", "color4"];
 
 let board = defaultBoard;
-let playing = false;
+let playing = true;
 let gamePattern = [];
 let playerColor = "";
-let turn = 'X';
+let turn = "X";
+let turnCount = 1;
 
 // detect keypress to begin game -- CHANGE THIS, MAKE ALL MOUSE/TOUCH ONLY
 document.addEventListener("keydown", function () {
+  if (playing === false) {
+    // if game is not in progress, initialise it
 
-    if (playing === false) {
-  
-      // if game is not in progress, initialise it
-  
-      playing = true; // i.e. let's begin a game
-      board = defaultBoard;
-      turn = 'X';
+    playing = true; // i.e. let's begin a game
+    board = defaultBoard;
+    turn = "X";
 
-      updateStatus("X to play");
+    updateStatus("X to play");
 
-      $(".cell").addClass("hoverable");
-  
-      setTimeout(function (){
-        gameColor = randomColor();
-        gamePattern.push(gameColor);
-        flashButton(gameColor);
-        playSound(gameColor);
-      }, 400); // pause in milliseconds
-    }
-  });
-  
-  
+    $(".cell").addClass("hoverable");
+
+    setTimeout(function () {
+      gameColor = randomColor();
+      gamePattern.push(gameColor);
+      flashButton(gameColor);
+      playSound(gameColor);
+    }, 400); // pause in milliseconds
+  }
+});
+
 //with game in progress, handle button clicks
-$(".btn").on("mousedown", function () {
+$(".cell").on("mousedown", function () {
+  thisCell = this.id;
+  row = thisCell[1];
+  col = thisCell[3];
 
-    if (playing === true) {
+  if (playing === true) {
+    // check the cell is available
+    if (board[row][col] === "-") {
+      board[row][col] = `${turn}`;
+      this.innerText = `${turn}`;
+      console.log(`${turnCount}: player ${turn} chose ${thisCell}`);
 
-      // grab the color which was clicked
-      thisCell = this.id;
-      console.log("cell:",thisCell,"turn:",turn);
+      // check win state
+      checkWin();
 
-        // check the cell is available
-        if (thisCell === available) {
-
-            playSound(click);
-
-            // update the board array
-            
-            // redraw the board
-
-            // check win state
-            if (thisItem === gamePattern.length) {
-                level += 1;          
-              }  
-                  
-            // switch to next player 
-  
-        
-        //if player has matched ALL colours, go to next level
-        checkWin();
-      } 
+      // swap to next player
+      if (playing === true) {
+        turnCount++;
+        if (turn == "X") {
+          turn = "O";
+          updateStatus("Player O to play");
+        } else {
+          turn = "X";
+          updateStatus("Player X to play");
+        }
+      }
     }
-  });
-  
-  
+  }
+});
 
 function updateBoard() {
-    for (row=0;row<=3;row++) {
-        for (col=0;col<=3;col++) {
-            console.log(`r${row}c${col} = ${board[row][col]}`);
-            thisElement = document.getElementById("r"+row+"c"+col);
-            thisElement.textContent = board[row][col];
-        }
+  for (row = 0; row <= 3; row++) {
+    for (col = 0; col <= 3; col++) {
+      // console.log(`r${row}c${col} = ${board[row][col]}`);
+      thisElement = document.getElementById("r" + row + "c" + col);
+      thisElement.innerText = board[row][col];
     }
+  }
 }
 
-function updateStatus (string) {
- $("#subtitle").text(string);
-}
+function checkWin() {
+  let thisString = "";
 
-function checkWin () {
-
-    console.log(board.join());
-
+  if (playing === true) {
     // check horizontals
-    for (row=0;row<=4;row++) {
-    thisRow = [board[row][0],board[row][1],board[row][2],board[row][3]];
-        if (thisRow.join() === "XXXX") {
-            console.log("Player X has won");
-        } else if (thisRow.join() === "OOOO") {
-            console.log("Player O has won");
-        }
+    for (row = 0; row <= 3; row++) {
+      for (col = 0; col <= 3; col++) {
+        thisString += board[row][col];
+      }
+
+    //   console.log(`Check row${row}: ${thisString}`);
+
+      if (thisString === "XXXX") {
+        updateStatus("Player X wins!");
+        playing = false;
+      } else if (thisString == "OOOO") {
+        updateStatus("Player O wins!");
+        playing = false;
+      } else if (turnCount == 16) {
+        updateStatus("Stalemate!");
+        playing = false;
+      }
+
+      thisString = "";
     }
 
     // check verticals
-    for (col=0;col<=4;col++) {
-        thisCol = [board[0][col],board[1][col],board[2][col],board[3][col]];
-        if (thisCol.join() === "XXXX") {
-            console.log("Player X has won");
-        } else if (thisCol.join() === "OOOO") {
-            console.log("Player O has won");
-        }
+    for (col = 0; col <= 3; col++) {
+      for (row = 0; row <= 3; row++) {
+        thisString += board[row][col];
+      }
+
+    //   console.log(`Check col${col}: ${thisString}`);
+
+      if (thisString === "XXXX") {
+        updateStatus("Player X wins!");
+        playing = false;
+      } else if (thisString == "OOOO") {
+        updateStatus("Player O wins!");
+        playing = false;
+      }
+
+      thisString = "";
     }
 
     // check diagonals
-    diag1 = [board[0][0],board[1][1],board[2][2],board[3][3]];
-    diag2 = [board[0][3],board[1][2],board[2][1],board[3][0]];
-    if (diag1.join() === "XXXX") {
-        console.log("Player X has won");
-    } else if (diag1.join() === "OOOO") {
-        console.log("Player O has won");
+    diag1 = board[0][0] + board[1][1] + board[2][2] + board[3][3];
+    diag2 = board[0][3] + board[1][2] + board[2][1] + board[3][0];
+    // console.log(`Check diag1: ${diag1}`);
+    // console.log(`Check diag2: ${diag2}`);
+    if (diag1 === "XXXX" || diag2 === "XXXX") {
+      updateStatus("Player X wins!");
+      playing = false;
+    } else if (diag1 === "OOOO" || diag2 === "OOOO") {
+      updateStatus("Player O wins!");
+      playing = false;
     }
-    if (diag2.join() === "XXXX") {
-        console.log("Player X has won");
-    } else if (diag2.join() === "OOOO") {
-        console.log("Player O has won");
-    }
-
+  }
 }
-  
-updateStatus("status goes here");
-updateBoard();
+
+function updateStatus(string) {
+  $("#subtitle").text(string);
+}
+
+updateStatus("Player X to play");
