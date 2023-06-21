48f025b248f1f6ae782d8a88a1b5a415abbc4184
Nick S <nickswinglehurst@gmail.com>
2023-06-21 08:40:48 +0100

    LTW layout and WIP localStorage

--- lefttowrite/script.js
+++ lefttowrite/script.js
@@ -1,24 +1,57 @@
 $(document).ready( function() {
+
     setCopyrightYear();
 
     // default today field to today's date
     let todayDate = new Date();
     document.getElementById('date-today').valueAsDate = todayDate;
 
-    // default startDate to 10 days before todayDate
-    let startDate = new Date(todayDate);
-    startDate.setDate(startDate.getDate() - 10);
+    // load startDate from localStorage
+    let startDate = todayDate;
+    if (localStorage.getItem("startDate") === null) {
+        // if none saved, default to 10 days before todayDate
+        startDate.setDate(todayDate.getDate() - 10);
+        localStorage.setItem("startDate",startDate);
+        console.log("SET startDate:",localStorage.setItem("startDate"));
+    } else {
+        // load it from local Storage
+        let startDate = new Date(localStorage.getItem("startDate"));
+        console.log("GET startDate:",localStorage.getItem("startDate"));
+
+    }
     document.getElementById('date-start').valueAsDate = startDate;
 
-    // default targetDate to 30 days after todayDate
-    let targetDate = new Date(todayDate);
-    targetDate.setDate(targetDate.getDate() + 30);
+    // load targetDate from localStorage
+    let targetDate = new Date();
+    if (localStorage.getItem("targetDate") != null) {
+        targetDate = new Date(localStorage.getItem("targetDate"));
+    } else {
+        // otherwise default it to 30 days after todayDate
+        targetDate = new Date(todayDate);
+        targetDate.setDate(targetDate.getDate() + 30);
+    }
     document.getElementById('date-target').valueAsDate = targetDate;
     
-    // set initial values for other fields
-    document.getElementById('words-current').value = 5000;
+    // load currentWords from local storage
+    if (localStorage.getItem("currentWords") != null) {
+        currentWords = new Date(localStorage.getItem("currentWords"));
+        document.getElementById('words-current').value = currentWords;
+    } else {
+        // otherwise default it to 5000
+        document.getElementById('words-current').value = 5000;
+    }
 
-    $('#words-target').val(10000);
+    // load targetWords from local storage
+    let targetWords = 10000;
+    if (localStorage.getItem("currentWords") != null) {
+        targetWords = new Date(localStorage.getItem("currentWords"));
+        document.getElementById('words-target').value = targetWords;
+    } else {
+        // otherwise default it to 10000
+        document.getElementById('words-target').value = 10000;
+    }
+
+    
     $('#days-current').val(10);
     $("#rate-current").text("");
     $("#date-predicted").text(String(todayDate).slice(0, 15));
@@ -38,9 +71,11 @@ function update() {
     if(document.getElementById('choose-days-current').checked) {
         // if user has selected number of days
         daysElapsed = document.getElementById('days-current').value;
+        localStorage.setItem("daysElapsed",daysElapsed);
     } else if(document.getElementById('choose-date-start').checked) {
         // if user has selected a start date
         startDate = document.getElementById('date-start').valueAsDate;
+        localStorage.setItem("startDate", startDate);
         daysElapsed = diff_in_days(startDate, todayDate);
     }
     
@@ -55,6 +90,7 @@ function update() {
 
     // calculate predicted finish date
     targetWords = document.getElementById('words-target').value;
+    localStorage.setItem("targetWords", targetWords);
     predictedDate = new Date(todayDate);
     addedDays = (targetWords - currentWords) / currentRate;
     predictedDate.setDate(predictedDate.getDate() + addedDays);
@@ -62,6 +98,7 @@ function update() {
 
     // calculate required rate
     targetDate = document.getElementById('date-target').valueAsDate;
+    localStorage.setItem("targetDate", targetDate);
     daysRemaining = diff_in_days(targetDate,todayDate);
     requiredRate = (targetWords - currentWords) / daysRemaining;
     if (isFinite(requiredRate)) {
@@ -71,7 +108,10 @@ function update() {
     }
 }
 
-// calculate the absolute difference in days between two dates
+
+/* general useful functions */
+
+// return absolute difference in days between two dates
 function diff_in_days(date1, date2) {
     dt1 = new Date(date1);
     dt2 = new Date(date2);
@@ -79,17 +119,10 @@ function diff_in_days(date1, date2) {
     return Math.abs(difference);
 }
 
-function getDayName(dateStr, locale) {
-    var date = new Date(dateStr);
-    return date.toLocaleDateString(locale, { weekday: 'long' });        
-}
-
-// returns a nice date string, e.g. 'Saturday 22nd July 2021'
+// return a nice date string, e.g. 'Saturday 22nd July 2021'
 function niceDate(thisDate) { 
-
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday","Saturday","Sunday"];
     const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-
     thisDayOfWeek = days[thisDate.getUTCDay()];
     thisDayNumber = thisDate.getUTCDate();
     
@@ -109,7 +142,7 @@ function niceDate(thisDate) {
             suffix = "th";
     }
 
-    thisMonth = months[(thisDate.getUTCMonth() + 1)];
+    thisMonth = months[(thisDate.getMonth() + 1)];
     thisYear = thisDate.getUTCFullYear();
     dateString = `${thisDayOfWeek} ${thisDayNumber}${suffix} ${thisMonth} ${thisYear}`;
     return dateString
