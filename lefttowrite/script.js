943649051cedfeb218808e03842132820c2111bf
Nick S <nickswinglehurst@gmail.com>
2023-06-22 16:44:27 +0100

    LTW implemented local storage and form reset

--- lefttowrite/script.js
+++ lefttowrite/script.js
@@ -1,115 +1,168 @@
-$(document).ready( function() {
+// on DOM loading initialise fields and local storage
+document.addEventListener("DOMContentLoaded", () => {
 
+    console.log("Initialising on DOM load")
     setCopyrightYear();
 
     // default today field to today's date
     let todayDate = new Date();
     document.getElementById('date-today').valueAsDate = todayDate;
 
-    // load startDate from localStorage
+    // check localStorage and update fields accordingly
+
+    // verify localStorage
+    if (localStorage.length == 0) {
+        console.log("localStorage on load is empty")
+    } else {
+        for (i=0;i<=localStorage.length;i++){
+            console.log("localStorage on load:",localStorage.key(i)+":",localStorage.getItem(localStorage.key(i)));
+        }
+    }
+
+    // check for currentWords
+    if (localStorage.getItem("currentWords") == null) {
+        // none saved, SET default value
+        currentWords = 1000;
+        localStorage.setItem("currentWords",currentWords);
+        console.log("DEFAULT currentWords",currentWords);
+    } else {
+        // else GET currentWords from storage
+        currentWords = localStorage.getItem("currentWords");
+    }
+    document.getElementById('words-current').value = currentWords;
+
+    // check for targetWords
+    if (localStorage.getItem("targetWords") == null) {
+        // none saved, SET default value
+        targetWords = 5000;
+        localStorage.setItem("targetWords",targetWords);
+        console.log("DEFAULT targetWords",targetWords);
+    } else {
+        // else GET targetWords from storage
+        targetWords = localStorage.getItem("targetWords");
+    }
+    document.getElementById('words-target').value = targetWords;
+
+    // check for daysPerWeek - NOT YET IMPLEMENTED
+    // if (localStorage.getItem("daysPerWeek") == null) {
+    //     // none saved, SET default value7
+    //     daysPerWeek = 7;
+    //     localStorage.setItem("daysPerWeek",daysPerWeek);
+    // } else {
+    //     // else GET daysPerWeek from storage
+    //     daysPerWeek = localStorage.getItem("daysPerWeek");
+    // }
+    // document.getElementById('days-per-week').value = daysPerWeek;
+
+    // check daysToDate
+    if (localStorage.getItem("daysToDate") == null) {
+        // none saved, SET default value
+        daysToDate = 10;
+        localStorage.setItem("daysToDate",daysToDate);
+        console.log("DEFAULT daysToDate",daysToDate);
+    } else {
+        // else GET daysToDate from storage
+        daysToDate = localStorage.getItem("daysToDate");
+    }
+    document.getElementById('days-to-date').value = daysToDate;
+
+    // check startDate
     let startDate = todayDate;
-    if (localStorage.getItem("startDate") === null) {
-        // if none saved, default to 10 days before todayDate
+    if (localStorage.getItem("startDate") == null) {
+        // none saved, set default (today - 10 days)
         startDate.setDate(todayDate.getDate() - 10);
         localStorage.setItem("startDate",startDate);
-        console.log("SET startDate:",localStorage.setItem("startDate"));
+        console.log("DEFAULT startDate",startDate);
+        // console.log("SET startDate:",localStorage.setItem("startDate",startDate));
     } else {
         // load it from local Storage
-        let startDate = new Date(localStorage.getItem("startDate"));
-        console.log("GET startDate:",localStorage.getItem("startDate"));
-
+        startDate = new Date(localStorage.getItem("startDate"));
+        // console.log("GET startDate:",localStorage.getItem("startDate"));
     }
     document.getElementById('date-start').valueAsDate = startDate;
 
-    // load targetDate from localStorage
-    let targetDate = new Date();
-    if (localStorage.getItem("targetDate") != null) {
-        targetDate = new Date(localStorage.getItem("targetDate"));
+    let targetDate = todayDate;
+    if (localStorage.getItem("targetDate") == null) {
+        // none saved, set default (today + 30 days)
+        startDate.setDate(todayDate.getDate() + 30);
+        localStorage.setItem("targetDate",targetDate);
+        console.log("DEFAULT targetDate",targetDate);
     } else {
-        // otherwise default it to 30 days after todayDate
-        targetDate = new Date(todayDate);
-        targetDate.setDate(targetDate.getDate() + 30);
+        // load it from local Storage
+        targetDate = new Date(localStorage.getItem("targetDate"));
     }
     document.getElementById('date-target').valueAsDate = targetDate;
-    
-    // load currentWords from local storage
-    if (localStorage.getItem("currentWords") != null) {
-        currentWords = new Date(localStorage.getItem("currentWords"));
-        document.getElementById('words-current').value = currentWords;
-    } else {
-        // otherwise default it to 5000
-        document.getElementById('words-current').value = 5000;
-    }
 
-    // load targetWords from local storage
-    let targetWords = 10000;
-    if (localStorage.getItem("currentWords") != null) {
-        targetWords = new Date(localStorage.getItem("currentWords"));
-        document.getElementById('words-target').value = targetWords;
-    } else {
-        // otherwise default it to 10000
-        document.getElementById('words-target').value = 10000;
-    }
-
-    
-    $('#days-current').val(10);
-    $("#rate-current").text("");
-    $("#date-predicted").text(String(todayDate).slice(0, 15));
-    $("#rate-required").text("");
+    // set non-localStorage values to placeholder
+    document.getElementById("rate-current").innerHTML = "TBD";
+    document.getElementById("date-predicted").innerHTML = "TBD";
+    document.getElementById("rate-required").innerHTML = "TBD";
 
     update();
 });
 
+
+/* RECALCULATE OUTPUT VALUES WHEN ANY FIELD IS UPDATED */
+
 function update() {
 
-    // daysPerWeekField = $('#days-per-week option:selected');
-
-    // this lets user change the current date, say if they are planning ahead
+    // user can change the current date, say if they are planning ahead
     todayDate = document.getElementById('date-today').valueAsDate;
 
-    // calculate daysElapsed
-    if(document.getElementById('choose-days-current').checked) {
+    // grab and save current field values
+    currentWords = document.getElementById('words-current').value;
+    localStorage.setItem("currentWords", currentWords);
+    targetWords = document.getElementById('words-target').value;
+    localStorage.setItem("targetWords", targetWords);
+    daysToDate = document.getElementById('days-to-date').value;
+    localStorage.setItem("daysToDate",daysToDate);
+    startDate = document.getElementById('date-start').valueAsDate;
+    localStorage.setItem("startDate", startDate);
+    targetDate = document.getElementById('date-target').valueAsDate;
+    localStorage.setItem("targetDate", targetDate);
+
+    // daysPerWeek -- NOT IMPLEMENTED
+    // daysPerWeek = document.getElementById('days-per-week option:selected').value;
+    // localStorage.setItem("daysPerWeek", daysPerWeek);
+
+    // calculate daysElapsed - either daysToDate, or days between startDate and todayDate
+    if(document.getElementById('choose-days-to-date').checked) {
         // if user has selected number of days
-        daysElapsed = document.getElementById('days-current').value;
-        localStorage.setItem("daysElapsed",daysElapsed);
+        daysElapsed = daysToDate;
     } else if(document.getElementById('choose-date-start').checked) {
         // if user has selected a start date
-        startDate = document.getElementById('date-start').valueAsDate;
-        localStorage.setItem("startDate", startDate);
         daysElapsed = diff_in_days(startDate, todayDate);
     }
     
     // calculate currentRate
-    currentWords = document.getElementById('words-current').value;
     currentRate = (currentWords / daysElapsed);
     if (isFinite(currentRate)) {
-        $("#rate-current").text(Math.floor(currentRate));
+        document.getElementById("rate-current").innerHTML = Math.floor(currentRate);
     } else {
-        $("#rate-current").text("TBD");
+        document.getElementById("rate-current").innerHTML = "∞";
     }
 
     // calculate predicted finish date
-    targetWords = document.getElementById('words-target').value;
-    localStorage.setItem("targetWords", targetWords);
     predictedDate = new Date(todayDate);
     addedDays = (targetWords - currentWords) / currentRate;
     predictedDate.setDate(predictedDate.getDate() + addedDays);
-    $('#date-predicted').text(niceDate(predictedDate));
+    document.getElementById('date-predicted').innerHTML = niceDate(predictedDate);
 
     // calculate required rate
-    targetDate = document.getElementById('date-target').valueAsDate;
-    localStorage.setItem("targetDate", targetDate);
     daysRemaining = diff_in_days(targetDate,todayDate);
     requiredRate = (targetWords - currentWords) / daysRemaining;
     if (isFinite(requiredRate)) {
-        $("#rate-required").text(Math.floor(requiredRate));
+        document.getElementById("rate-required").innerHTML = Math.floor(requiredRate);
     } else {
-        $("#rate-required").text("TBD");
+        document.getElementById("rate-required").innerHTML = "∞";
     }
 }
 
-
-/* general useful functions */
+// clear localstorage
+document.getElementById('reset').onclick = function(){
+    localStorage.clear();
+    location.reload();
+}
 
 // return absolute difference in days between two dates
 function diff_in_days(date1, date2) {
@@ -121,12 +174,14 @@ function diff_in_days(date1, date2) {
 
 // return a nice date string, e.g. 'Saturday 22nd July 2021'
 function niceDate(thisDate) { 
-    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday","Saturday","Sunday"];
-    const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-    thisDayOfWeek = days[thisDate.getUTCDay()];
-    thisDayNumber = thisDate.getUTCDate();
+
+    // get formatted strings
+    thisYear = thisDate.getUTCFullYear();
+    thisMonth = thisDate.toLocaleDateString('en-UK', {month: 'long' });
+    thisDayNumber = thisDate.toLocaleDateString('en-UK', {day: 'numeric'});
+    thisDayOfWeek = thisDate.toLocaleDateString('en-UK', {weekday: 'long' });
     
-    // add suffix to day
+    // get ordinal suffix
     let lastDigit = thisDayNumber.toString().slice(-1);
     switch (lastDigit) {
         case "1":
@@ -142,8 +197,6 @@ function niceDate(thisDate) {
             suffix = "th";
     }
 
-    thisMonth = months[(thisDate.getMonth() + 1)];
-    thisYear = thisDate.getUTCFullYear();
     dateString = `${thisDayOfWeek} ${thisDayNumber}${suffix} ${thisMonth} ${thisYear}`;
     return dateString
 }
