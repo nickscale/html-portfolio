81786b16cfe703e83aad47a3cd58ffb37e1129db
Nick S <nickswinglehurst@gmail.com>
2023-06-28 07:34:58 +0100

    chime perco tests

--- chime/script.js
+++ chime/script.js
@@ -10,15 +10,16 @@ const pattern = [
   [0, 0, 0, 0, 0, 0, 0, 0],
 ];
 const percussion = [
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
 ];
+
 let nowPlaying = false;
 let playing;
 let currentBeat = 0;
@@ -26,7 +27,7 @@ let beatLength = 250; // ms
 
 $(document).ready(function () {
   $(".button").addClass("hoverable");
-  $(".button").text(""); // disable this to show cell ids 
+  $(".button").text(""); // disable this to show cell ids
   //   playNote(0);
 });
 
@@ -44,9 +45,9 @@ $(".button").on("click", function () {
   thisArray = pattern[thisBeat];
 
   // if already selected, unselect it
-  if ( $(this).hasClass("selected") ) {
+  if ($(this).hasClass("selected")) {
     $(this).removeClass("selected");
-    thisArray[thisPitch] = 0
+    thisArray[thisPitch] = 0;
   } else {
     $(this).addClass("selected");
     thisArray[thisPitch] = 1;
@@ -70,6 +71,85 @@ $(".button").on("mouseleave", function () {
   $(this).removeClass("highlighted");
 });
 
+function playPERCNote(perc) {
+  switch (Number(perc)) {
+    case 1:
+      var newPerc = new Audio("./sounds/snare.mp3");
+      break;
+    case 2:
+      var newPerc = new Audio("./sounds/closed-hi-hat.mp3");
+      break;
+    case 3:
+      var newPerc = new Audio("./sounds/open-hi-hat.mp3");
+      break;
+    case 4:
+      var newPerc = new Audio("./sounds/clap.mp3");
+      break;
+    case 5:
+      var newPerc = new Audio("./sounds/tambourine.wav");
+      break;
+    case 6:
+      var newPerc = new Audio("./sounds/FX1.mp3");
+      break;
+    case 7:
+      var newPerc = new Audio("./sounds/FX1.mp3");
+      break;
+    default:
+      var newPerc = new Audio("./sounds/kick.mp3");
+      break;
+  }
+  newPerc.play();
+}
+
+// play through currently selected PERCUSSION notes
+function playPERCPattern() {
+  if (nowPlaying == false) {
+    currentBeat = 0;
+    nowPlaying = true;
+
+    // toggle play/pause button
+    $("#btn-play").css("display", "none");
+    $("#btn-pause").css("display", "inline-block");
+
+    // start playing with interval timer
+    playing = setInterval(function () {
+      console.log(`Playing pattern${currentBeat}=${pattern[currentBeat]}`);
+
+      // find selected pitch at this beat
+      thisPitch = pattern[currentBeat].indexOf(1);
+
+      // highlight the buttons on the current beat
+      $(".button").removeClass("highlighted");
+      $(`.button[id^="b0${currentBeat}"]`).addClass("highlighted");
+
+      // only play note if there is a note is selected
+      if (thisPitch >= 0) {
+        playNote(thisPitch);
+      }
+
+      // loop from beat 7 back to beat 0
+      if (currentBeat == 7) {
+        currentBeat = 0;
+      } else {
+        currentBeat++;
+      }
+    }, beatLength);
+  } else if (nowPlaying == true) {
+    // if playing, stop playing
+    clearInterval(playing);
+    nowPlaying = false;
+    console.log("Stop playing");
+
+    // toggle play/pause button, remove highlighting
+    $("#btn-play").css("display", "inline-block");
+    $("#btn-pause").css("display", "none");
+    $(".button").removeClass("highlighted");
+
+    return;
+  }
+}
+
+// play notes at appropriate pitch
 function playNote(pitch = 0) {
   var newSound = new Audio("./sounds/Synth_C (5)-single.mp3");
   newSound.preservesPitch = false;
@@ -122,7 +202,7 @@ function playPattern() {
       thisPitch = pattern[currentBeat].indexOf(1);
 
       // highlight the buttons on the current beat
-      $('.button').removeClass("highlighted");
+      $(".button").removeClass("highlighted");
       $(`.button[id^="b0${currentBeat}"]`).addClass("highlighted");
 
       // only play note if there is a note is selected
@@ -154,50 +234,28 @@ function playPattern() {
 
 // randomly choose some notes
 function randomise() {
-    // clear current button selection
-    $(`.button`).removeClass("selected");
-    
-    for (i=0;i<=7;i++) {
-        // clear current note
-        pattern[i].fill(0,0,8);
+  // clear current button selection
+  $(`.button`).removeClass("selected");
 
-        // choose a pitch at random (8 = leave empty)
-        randomPitch = Math.floor(Math.random()*9);
-        console.log(`Random pattern${i}=${pattern[i]}`);
-        if (randomPitch <= 7) {
-            pattern[i][randomPitch] = 1;
-            $(`#b0${i}p0${randomPitch}`).addClass("selected");
-        }
+  for (i = 0; i <= 7; i++) {
+    // clear current note
+    pattern[i].fill(0, 0, 8);
+
+    // choose a pitch at random (8 = leave empty)
+    randomPitch = Math.floor(Math.random() * 9);
+    console.log(`Random pattern${i}=${pattern[i]}`);
+    if (randomPitch <= 7) {
+      pattern[i][randomPitch] = 1;
+      $(`#b0${i}p0${randomPitch}`).addClass("selected");
     }
+  }
 }
 
 // clear all current notes
 function clearPattern() {
-    console.log(`Clear pattern`);
-    $(`.button`).removeClass("selected");
-    for (i=0;i<=7;i++) {
-        pattern[i].fill(0,0,8);
-    }
+  console.log(`Clear pattern`);
+  $(`.button`).removeClass("selected");
+  for (i = 0; i <= 7; i++) {
+    pattern[i].fill(0, 0, 8);
+  }
 }
-
-/* using Web_audio_API */
-
-// const audioCtx = new AudioContext();
-// const wave = new PeriodicWave(audioCtx, {
-//     real: wavetable.real,
-//     imag: wavetable.imag,
-//   });
-
-// ctx.decodeAudioData(audioData).then((decodedData) => {
-//     // use the decoded data here
-// });
-
-// setTimeout(function (){
-//     gameColor = randomColor();
-//     gamePattern.push(gameColor);
-//     flashButton(gameColor);
-//     playSound(gameColor);
-//     $("button h1").text("Level " + String(level+1));
-//   }, 300); // pause in milliseconds
-
-// }
