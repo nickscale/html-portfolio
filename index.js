199d1e07ec5ad2ac123d27fb2cd4085b3fccd312
Nick S <nickswinglehurst@gmail.com>
2023-07-12 21:31:42 +0100

    add theme buttons

--- index.js
+++
@@ -1,9 +0,0 @@
-/* Add/remove "responsive" class to top-nav */
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
---
+++ main.js
@@ -0,0 +1,28 @@
+const colorThemes = document.querySelectorAll('[name="theme"]');
+
+// store the chosen theme
+const storeTheme = function (theme) {
+  localStorage.setItem("theme", theme);
+};
+
+// retrieve and set the theme
+const setTheme = function () {
+  const activeTheme = localStorage.getItem("theme");
+  console.log(activeTheme);
+  colorThemes.forEach((themeOption) => {
+    if (themeOption.id === activeTheme) {
+      themeOption.checked = true;
+    }
+  });
+  // fallback for browsers without :has support
+  document.documentElement.className = theme;
+};
+
+// check for clicks on theme picker
+colorThemes.forEach((themeOption) => {
+  themeOption.addEventListener("click", () => {
+    storeTheme(themeOption.id);
+  });
+});
+
+document.onload = setTheme();
