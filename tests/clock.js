6ad985bc8df116eaa52801cf0a8380bbae891d14
Nick S <nickswinglehurst@gmail.com>
2023-07-13 21:02:16 +0100

    test layouts

--- tests/clock.js
+++ tests/clock.js
@@ -2,23 +2,23 @@ let now = new Date();
 let hours = now.getHours();
 let minutes = now.getMinutes();
 let seconds = now.getSeconds();
-console.log(hours, minutes, seconds);
+console.log(hours+':'+minutes+':'+seconds);
 
 setTime(seconds, "second");
 setTime(minutes, "minute");
-setTime(hours, "hour");
+setTime((hours%12) + (minutes/60), "hour");
 
-function setTime(left, hand) {
+function setTime(angle, hand) {
     switch(hand) {
         case 'second':
-            offset = -6 * left;
+            offset = 6 * angle;
             break;
         case 'minute':
-            offset = 6 * left;
+            offset = 6 * angle;
             break;
         case 'hour':
-            offset = 30 * left;           
+            offset = 30 * angle;           
             break;
     }
-  document.querySelector("div.offset__"+hand).style.cssText = "transform: rotate("+offset+"deg);";
+  document.querySelector("div.offset_"+hand).style.cssText = "transform: rotate("+offset+"deg);";
 }
